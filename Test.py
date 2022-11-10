c28362d6672a739a67f4e79458c64211305bd7af
Ilia <forgotten-devil@mail.ru>
2022-11-11 01:20:38 +0500

    add one string to print and add class Student

--- Test.py
+++ Test.py
@@ -1,2 +1,10 @@
 print('Привет сокурсникам- для ошибки')
 print('Great training')
+
+print('New code')
+
+class Student():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
